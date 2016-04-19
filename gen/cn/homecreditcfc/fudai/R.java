308b20f2bdec360ebaeb22246ccb4bd7354bbeb6
Linbao <513058932@qq.com>
2016-04-19 23:56:35 +0800

    refectoring startup view and add animation to startup window

--- gen/cn/homecreditcfc/fudai/R.java
+++ gen/cn/homecreditcfc/fudai/R.java
@@ -8,6 +8,9 @@
 package cn.homecreditcfc.fudai;
 
 public final class R {
+    public static final class anim {
+        public static final int loading=0x7f040000;
+    }
     public static final class attr {
         /** <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
 or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
@@ -19,7 +22,7 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int metaButtonBarStyle=0x7f010000;
     }
     public static final class color {
-        public static final int black_overlay=0x7f040000;
+        public static final int black_overlay=0x7f050000;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -29,32 +32,38 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int wallet=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f090001;
-        public static final int fullscreen_content=0x7f090000;
+        public static final int LinearLayout1=0x7f0a0000;
+        public static final int action_settings=0x7f0a0005;
+        public static final int frame_layout_main=0x7f0a0004;
+        public static final int loading=0x7f0a0003;
+        public static final int wallet=0x7f0a0002;
+        public static final int webView=0x7f0a0001;
     }
     public static final class layout {
         public static final int activity_fu_dai_main=0x7f030000;
         public static final int activity_splasha=0x7f030001;
     }
     public static final class menu {
-        public static final int fu_dai_main=0x7f080000;
+        public static final int fu_dai_main=0x7f090000;
     }
     public static final class string {
-        public static final int action_settings=0x7f060005;
-        public static final int app_name=0x7f060000;
-        public static final int dummy_button=0x7f060001;
-        public static final int dummy_content=0x7f060002;
-        public static final int fudaiWalletLogo=0x7f060006;
-        public static final int hello_world=0x7f060004;
-        public static final int title_activity_fu_dai_main=0x7f060003;
+        public static final int action_settings=0x7f070005;
+        public static final int app_name=0x7f070000;
+        public static final int dummy_button=0x7f070001;
+        public static final int dummy_content=0x7f070002;
+        public static final int fudaiWalletLogo=0x7f070006;
+        public static final int hello_world=0x7f070004;
+        public static final int loadingDesc=0x7f070008;
+        public static final int title_activity_fu_dai_main=0x7f070003;
+        public static final int title_activity_main=0x7f070007;
     }
     public static final class style {
         /** 
@@ -81,21 +90,21 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
-        public static final int ButtonBar=0x7f070003;
-        public static final int ButtonBarButton=0x7f070004;
-        public static final int FullscreenActionBarStyle=0x7f070005;
+        public static final int AppTheme=0x7f080001;
+        public static final int ButtonBar=0x7f080003;
+        public static final int ButtonBarButton=0x7f080004;
+        public static final int FullscreenActionBarStyle=0x7f080005;
         /**  <item name="metaButtonBarStyle">@style/ButtonBar</item>
         <item name="metaButtonBarButtonStyle">@style/ButtonBarButton</item>
          
  <item name="metaButtonBarStyle">?android:attr/buttonBarStyle</item>
         <item name="metaButtonBarButtonStyle">?android:attr/buttonBarButtonStyle</item> 
          */
-        public static final int FullscreenTheme=0x7f070002;
+        public static final int FullscreenTheme=0x7f080002;
     }
     public static final class styleable {
         /** 
---
+++ src/cn/homecreditcfc/fudai/SplashActivity.java
@@ -0,0 +1,65 @@
+package cn.homecreditcfc.fudai;
+
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import cn.homecreditcfc.fudai.util.SystemUiHider;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ *
+ * @see SystemUiHider
+ */
+public class SplashActivity extends Activity {
+	/**
+	 * Whether or not the system UI should be auto-hidden after
+	 * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
+	 */
+	private static final boolean AUTO_HIDE = true;
+
+	/**
+	 * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
+	 * user interaction before hiding the system UI.
+	 */
+	private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+
+	/**
+	 * If set, will toggle the system UI visibility upon interaction. Otherwise,
+	 * will show the system UI visibility upon interaction.
+	 */
+	private static final boolean TOGGLE_ON_CLICK = true;
+
+	/**
+	 * The flags to pass to {@link SystemUiHider#getInstance}.
+	 */
+	private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
+
+	/**
+	 * The instance of the {@link SystemUiHider} for this activity.
+	 */
+	private SystemUiHider systemUiHider;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_splasha);
+		final View contentView = findViewById(R.id.wallet);
+		
+		
+	}
+}
+
